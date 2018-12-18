709bb396d1e578abb91847ec5ff45250278c092f
ivan <ivanbacher@users.noreply.github.com>
2018-12-18 11:08:32 +0000

    router added

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 export class App {
-  constructor() {
-    this.message = 'Hello World!';
+
+  configureRouter(config, router) {
+    this.router = router;
+
+    config.title = 'Aurelia';
+
+    config.map([
+      { route: ['', 'page0'], name: 'page0', moduleId: 'page0.html' },
+      { route: 'page1', name: 'page1', moduleId: 'page1.html' }
+    ]);
   }
 }
